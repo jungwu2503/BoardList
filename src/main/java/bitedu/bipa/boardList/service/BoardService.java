35a67484f2ce74ed4508cda4c99724ecc71081be
kimmss00 <68334337+kimmss00@users.noreply.github.com>
2023-07-12 15:01:08 +0900

    BoardService

---
+++ src/main/java/bitedu/bipa/boardList/service/BoardService.java
@@ -0,0 +1,31 @@
+package bitedu.bipa.boardList.service;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import bitedu.bipa.boardList.dao.BoardDAO;
+import bitedu.bipa.boardList.vo.BoardVO;
+
+@Service
+public class BoardService {
+	
+	@Autowired
+	private BoardDAO dao;
+	
+	//search list all
+	public ArrayList<BoardVO> searchAll(){
+		System.out.println("search");
+		ArrayList<BoardVO> list = dao.searchAll();
+		return list;
+	}
+	
+	public boolean deleteBoard(String no) {
+		boolean flag = false;
+		System.out.println("delete");		
+		flag = dao.delete(Integer.parseInt(no));
+		return flag;
+	}
+
+}
